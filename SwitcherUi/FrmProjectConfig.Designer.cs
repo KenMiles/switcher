d7b20fe1ddeab99d1ac63c9197fa5bbafbb3634e
Ken <github@kenmiles.co.uk>
2015-07-26 21:26:33 +0100

    Um Not sure whatt happened here :s

---
+++ SwitcherUi/FrmProjectConfig.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace SwitcherUi.switching.cfg
+{
+    partial class FrmProjectConfig
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FrmProjectConfig
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1004, 424);
+            this.Name = "FrmProjectConfig";
+            this.Text = "FrmProjectConfig";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
